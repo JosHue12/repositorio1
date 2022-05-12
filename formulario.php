79e1e50f4c264ead3c16a957ad9660bfdcff3586
jose <jpedrohuerta14@gmail.com>
2022-05-12 13:55:31 -0300

    "form added"

---
+++ formulario.php
@@ -0,0 +1,4 @@
+<form action = "datos.php" method = "get">
+    <input type = "text" name = "nombre" placeholder = "nombre">
+    <input type = "email" name = "correo" placeholder = "pepe@gmail.com">
+</form>
